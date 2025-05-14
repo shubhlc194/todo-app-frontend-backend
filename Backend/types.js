24572eb941cc2d2b170470ed9662c2333656a0e6
shubham chaudhary <142807471+shubhlc194@users.noreply.github.com>
2025-05-14 16:41:03 +0530

    Update types.js

---
+++ Backend/types.js
@@ -0,0 +1,17 @@
+
+const zod=require("zod");
+  /*
+     title:string
+     description:string
+  */
+ const createtodo=zod.object({
+  title:zod.string(),
+  description: zod.string() 
+ })
+  const updatetodo=zod.object({
+  id:zod.string()
+ })
+ module.exports={
+    createtodo:createtodo,
+    updatetodo:updatetodo
+ }
