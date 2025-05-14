c417a1aa1272e17aabeadc43a4af7ca456490cd4
shubhlc194 <shubhamkc194@gmail.com>
2025-05-14 16:39:03 +0530

    waise to ye push nhi karna chahiye but kar deta hu for learning phase

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,30 @@
 const express = require("express");
+const { createtodo }    = require("./types")
 const app = express();
 const port = 3000;
+app.use(express.json());
+// //body:
+// title:string
+// description:string
+app.post("/todo",function(req,res){
+    const createpayload=req.body;
+    const parsepayload=createtodo.safeParse(createpayload)
+    if(!parsepayload.success){
+       return res.status(411).json({
+        msg:"you send a wrong input "
+       }) 
+       return; 
+    }
+    
+})
+app.get("/todos",(req,res)=>{
 
+})
+app.put("/completed",(req,res)=>{
+
+})
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+  
