ae98147a53d4237e7c5925752df96374e62d5d1c
shubhlc194 <shubhamkc194@gmail.com>
2025-05-14 19:07:18 +0530

    frontend

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,8 +1,19 @@
 
 export function CreateTodo(){
     return <div>
-<input type="text" placeholder="title"></input> <br></br>
-<input type="text" placeholder="description"></input><br></br>
-<button> add a todo</button>
+<input style={{
+        padding:10,
+        margin:10
+    }}
+type="text" placeholder="title"></input> <br></br>
+<input style={{
+        padding:10,
+        margin:10
+    }}
+type="text" placeholder="description"></input><br></br>
+<button style={{
+        padding:10,
+        margin:10
+    }}> add a todo</button>
     </div>
 }
---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,9 @@
+
+export function Todos(){
+    return <div>
+   <h1> go to gym</h1>
+   <h2> you need to go to gym</h2>
+   <button> Mark as completed</button>
+
+    </div>
+}
